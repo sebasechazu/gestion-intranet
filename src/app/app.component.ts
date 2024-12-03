5ae4168b5237837bc966a8c5f96cc94b64a87190
Sebas Echazú <sebastianechazu@outlook.com>
2024-12-03 18:16:13 -0300

    feat: agregar un menú de navegación en la cabecera y mejorar la estructura del componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,52 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { MessageModule } from 'primeng/message';
 import { CommonModule } from '@angular/common';
+import { ButtonIcon } from 'primeng/button';
+import { Menubar } from 'primeng/menubar';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { Menu, MenuItemContent } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule, SidebarComponent,Menubar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  items: MenuItem[] | undefined;
   title = 'app';
+
+  ngOnInit() {
+    this.items = [
+      {
+        icon: 'pi pi-inbox'
+      },
+      {
+        
+        icon: 'pi pi-bell'
+      },
+      {
+        icon: 'pi pi-user',
+        items: [
+          {
+            label: 'Editar Perfil',
+            icon: 'pi pi-user-edit'
+          },
+          {
+            label: 'Datos',
+            icon: 'pi pi-server'
+          },
+          {
+            label: 'Salir',
+            icon: 'pi-sign-out'
+          },
+          
+        ]
+      },
+      
+    ]
+  }
 }
